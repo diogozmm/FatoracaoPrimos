3e8c4e6c798b5b81394c1d54c45f4940ab460904
Diogo Zimmermann <diogo.zmm@gmail.com>
2019-12-19 16:12:38 -0300

    Code Ready

---
+++ FatoracaoPrimos/Primos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FatoracaoPrimos
+{
+    class Primos
+    {
+        public static List<int> Fatorar(int numero)
+        {
+            var primos = new List<int>();
+
+            for (int div = 2; div <= numero; div++)
+            {
+                while (numero % div == 0)
+                {
+                    primos.Add(div);
+                    numero = numero / div;
+                }
+            }
+            return primos;
+        }
+
+     
+    }
+}
+
---
+++ FatoracaoPrimos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FatoracaoPrimos;
+
+namespace FatoracaoPrimos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número para ser fatorado: ");
+            int value = int.Parse(Console.ReadLine());
+
+            foreach(var i in Primos.Fatorar(value))
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+    }
+
+   
+}
+
